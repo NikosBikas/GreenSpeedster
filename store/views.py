0ebba096bd84a6aa39a8b5dcaf0fcd68510a3e56
ThrillSeeker01 <nikosbikas@hotmail.com>
2022-03-19 22:39:08 +0200

    Implement Product Detail Url and Design

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from tkinter import E
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
@@ -21,3 +22,14 @@ def store(request, category_slug=None):
         'product_count': product_count, 
     }
     return render(request, 'store/store.html', context)
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+
+    context = {
+    'single_product': single_product, 
+    }
+    return render(request, 'store/product_detail.html', context)
