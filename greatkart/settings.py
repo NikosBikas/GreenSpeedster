f2f67cc2b00ad1b38ac242ec2d91121955dce5ad
Nikolaos <nikosbikas@hotmail.com>
2022-04-12 01:40:25 +0300

    Merged Testing Branch to master

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from django.contrib.messages import constants as messages
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +133,16 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP CONFIGURATION
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'nick.dev.testing@gmail.com'
+EMAIL_HOST_PASSWORD = '!2#4%6&8(Nick'
+EMAIL_USE_TLS = True
