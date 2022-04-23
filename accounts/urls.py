dee122db371b89d98b7ab3c94f69e6fbb5273366
Nikolaos Bikas <nikosbikas@hotmail.com>
2022-04-23 13:15:12 +0300

    Fixed Navbar and paginator color hover conflicts with bootstrap

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
     path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate'),
     path('resetPassword/', views.resetPassword, name='resetPassword'),
 
+    path('my_orders/', views.my_orders, name='my_orders')
+
+
 
 
 
