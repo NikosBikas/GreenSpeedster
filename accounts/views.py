4c35dc3cb98a5a9cc725f81654510cfe76a5858e
pan1000 <pan.katsaras@gmail.com>
2022-04-27 23:26:18 +0300

    price slider deleted

--- accounts/views.py
+++ accounts/views.py
@@ -23,7 +23,7 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
             phone_number = form.cleaned_data['phone_number']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
